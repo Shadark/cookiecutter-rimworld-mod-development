f0d09f4e25208e0d32f45a3a74cefdcd577abbb3
L0laapk3 <L0laapk3@gmail.com>
2018-03-05 00:30:36 +0100

    Added some basic checks

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+
+pathlist = os.getcwd().split('\\')
+if pathlist[-2] != "Mods":
+    print "Needs to be ran inside the 'Mods' Directory of rimworld. Template aborted."
+    sys.exit(1)
+
+if not os.path.exists('\\'.join(pathlist[:-2]) + "\RimWorldWin.exe"):
+    print "Could not find '" '\\'.join(pathlist[:-2]) + "\RimWorldWin.exe'. Template aborted."
+    sys.exit(1)
