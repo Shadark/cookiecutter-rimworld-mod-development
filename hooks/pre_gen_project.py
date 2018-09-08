513cc046c767519ab7cf0e18bac56f9311654215
Shadark <fauglir@gmail.com>
2018-09-08 14:12:03 +0200

    Added 64-bit support

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -7,6 +7,6 @@ if pathlist[-2] != "Mods":
     print "Needs to be ran inside the 'Mods' Directory of rimworld. Template aborted."
     sys.exit(1)
 
-if not os.path.exists('\\'.join(pathlist[:-2]) + "\RimWorldWin.exe"):
-    print "Could not find '" '\\'.join(pathlist[:-2]) + "\RimWorldWin.exe'. Template aborted."
+if not os.path.exists('\\'.join(pathlist[:-2]) + "\RimWorldWin64.exe"):
+    print "Could not find '" '\\'.join(pathlist[:-2]) + "\RimWorldWin64.exe'. Template aborted."
     sys.exit(1)
